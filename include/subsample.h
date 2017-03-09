f7be540883af482b3a6bfd6dad882d78da23c556
Niklas Mähler <niklas.mahler@gmail.com>
2017-03-09 12:48:50 +0100

    Add subsampling function

--- include/subsample.h
+++ include/subsample.h
@@ -1,6 +1,8 @@
 #ifndef SUBSAMPLE_H
 #define SUBSAMPLE_H
 
-struct genotype *subsample_genotype(struct genotype *genotype, double maf, double max_mgf);
+#include "genotype.h"
+
+int subsample_genotype(struct o_genotype *ogt, double target_maf, double margin, double max_mgf);
 
 #endif
--- src/subsample.c
+++ src/subsample.c
@@ -4,6 +4,32 @@
 #include "subsample.h"
 #include "genotype.h"
 
-struct genotype *subsample_genotype(struct genotype *genotype, double maf, double max_mgf) {
-  return genotype;
+int subsample_genotype(struct o_genotype *ogt, double target_maf, double margin, double max_mgf) {
+  double maf;
+  double mgf;
+
+  maf = ogt_maf(ogt);
+  mgf = ogt_mgf(ogt);
+
+  if (mgf > max_mgf) {
+    return 1;
+  }
+
+  if (maf > target_maf - margin & maf < target_maf + margin) {
+    return 0;
+  }
+
+  if (maf < target_maf & ogt->hom_major > 0) {
+    ogt->hom_major--;
+    return subsample_genotype(ogt, target_maf, margin, max_mgf);
+  }
+  if (maf > target_maf & ogt->hom_minor > 0) {
+    ogt->hom_minor--;
+    return subsample_genotype(ogt, target_maf, margin, max_mgf);
+  }
+  if (maf > target_maf & ogt->het > 0) {
+    ogt->het--;
+    return subsample_genotype(ogt, target_maf, margin, max_mgf);
+  }
+  return 1;
 }
