a274d5b8349cb893311b016ed39f56ebf579d3a7
Niklas Mähler <niklas.mahler@gmail.com>
2017-03-09 12:47:21 +0100

    Add genotype struct based on the major allele
    In addition, more functionality surrounding the genotype structs is
    added, and it is possible to convert from genotype to o_genotype.

--- include/genotype.h
+++ include/genotype.h
@@ -1,11 +1,20 @@
 #ifndef GENOTYPE_H
 #define GENOTYPE_H
 
+// A genotype struct that is based on reference and alternative alleles
 struct genotype {
   unsigned int hom_ref, hom_alt, het;
 };
 
-double gt_maf(struct genotype*);
-double gt_mgf(struct genotype*);
+// A genotype struct that is based on major and minor alleles
+struct o_genotype {
+  unsigned int hom_major, hom_minor, het;
+};
+
+int gt_to_ogt(struct genotype *gt, struct o_genotype *ogt);
+double gt_maf(struct genotype *gt);
+double ogt_maf(struct o_genotype *ogt);
+double gt_mgf(struct genotype *gt);
+double ogt_mgf(struct o_genotype *ogt);
 
 #endif
--- src/genotype.c
+++ src/genotype.c
@@ -2,17 +2,43 @@
 
 #include "genotype.h"
 
-int count_samples(struct genotype *genotype) {
-  return genotype->hom_ref + genotype->hom_alt + genotype->het;
+int gt_count_samples(struct genotype *gt) {
+  return gt->hom_ref + gt->hom_alt + gt->het;
 }
 
-double gt_maf(struct genotype *genotype) {
-  double n_samples = count_samples(genotype);
-  double ref_freq = (2 * genotype->hom_ref + genotype->het) / (2 * n_samples);
+int ogt_count_samples(struct o_genotype *ogt) {
+  return ogt->hom_major + ogt->hom_minor + ogt->het;
+}
+
+int gt_to_ogt(struct genotype *gt, struct o_genotype *ogt) {
+  ogt->het = gt->het;
+  if (2 * gt->hom_ref + gt->het > 2 * gt->hom_alt + gt->het) {
+    ogt->hom_major = gt->hom_ref;
+    ogt->hom_minor = gt->hom_alt;
+  } else {
+    ogt->hom_major = gt->hom_alt;
+    ogt->hom_minor = gt->hom_ref;
+  }
+  return 0;
+}
+
+double gt_maf(struct genotype *gt) {
+  double n_samples = gt_count_samples(gt);
+  double ref_freq = (2 * gt->hom_ref + gt->het) / (2 * n_samples);
   return fmin(ref_freq, 1 - ref_freq);
 }
 
-double gt_mgf(struct genotype *genotype) {
-  double n_samples = count_samples(genotype);
-  return fmax(fmax(genotype->hom_ref, genotype->het), genotype->hom_alt) / n_samples;
+double ogt_maf(struct o_genotype *ogt) {
+  double n_samples = ogt_count_samples(ogt);
+  return (2 * ogt->hom_minor + ogt->het) / (2 * n_samples);
+}
+
+double gt_mgf(struct genotype *gt) {
+  double n_samples = gt_count_samples(gt);
+  return fmax(fmax(gt->hom_ref, gt->het), gt->hom_alt) / n_samples;
+}
+
+double ogt_mgf(struct o_genotype *ogt) {
+  double n_samples = ogt_count_samples(ogt);
+  return fmax(fmax(ogt->hom_major, ogt->het), ogt->hom_minor) / n_samples;
 }
